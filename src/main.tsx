eb725b200d4f1e05dd44138d8e32c614b5c45d67
José Cássio <josecassio2013@gmail.com>
2023-04-12 13:05:34 -0300

    use global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
+import GlobalStyles from './styles/global'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles/>
     <Home />
   </React.StrictMode>,
 )
