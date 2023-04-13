fbf544e4b69f8ba1e9dcda1174ae1bf31bf702c6
José Cássio <josecassio2013@gmail.com>
2023-04-13 10:34:48 -0300

    add api infos

--- src/pages/ApiInfo/index.tsx
+++ src/pages/ApiInfo/index.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import { useNavigate, redirect } from 'react-router-dom'
 import { toast } from "react-toastify";
 
-import { Container, Header } from "./styles";
+import { Container, Header, Main, Section, Form } from "./styles";
 
 import { api } from '../../services/api'
 
 import { Button } from "../../components/Button";
 import { TextButton } from "../../components/TextButton";
+import { Input } from "../../components/Input";
 
 interface  BadWordInterface{
   id: number;
@@ -16,32 +17,88 @@ interface  BadWordInterface{
 }
 
 export function ApiInfo() {
-  const [text, setText] = useState('');
+  const [title, setTitle] = useState('');
   const [words, setWords] = useState<BadWordInterface[]>([])
+  const [sugestions, setSugestions] = useState<BadWordInterface[]>([])
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  async function handleSendSugestion(){
+    if(!title){
+      return toast.warn("Digite algo para poder enviar a sugestão");
+    }
+
+    try{
+      await api.post('/sugestions', {title})
+      toast.success("Sugestão inserida! Atualize a página para vê-la")
+
+    }catch(error: any){
+        if(error.response){
+            toast.warn(error.response.data.message)
+        }else{
+            toast.warn("Não foi possível registrar uma sugestão");
+        }
+    }
+}
+
   useEffect(() => {
     async function fetchWords(){
        await api.get("/words?title=").then(({data}) => setWords(data));
     }
+    async function fetchSugestions(){
+      await api.get("/sugestions?title=").then(({data}) => setSugestions(data));
+   }
+
     fetchWords();
+    fetchSugestions();
+
     console.log(words)
+    console.log(sugestions)
   }, [])
+
 
   return(
     <Container>
+
       <Header>
         <TextButton onClick={() => navigate(-1)} text="voltar"/>
         <h1>Informações da <span>API</span></h1>
         <TextButton text="sobre" link="https://github.com/Jcassio-dev/ProfanityWords-API"/>
-     </Header>
+      </Header>
 
-     <h2>Palavrões catálogados até agora</h2>
-     <div>
-      {words && words.map(word => (
-        <span key={String(word.id)}>"{word.title}"</span>
-      ))}
-     </div>
+      <Main>
+       <h1>Catalogados</h1>
+
+        <div className="Sections">
+          <Section>
+              <h2>Palavrões</h2>
+              <div>
+                {words && words.map(word => (
+                  <span key={String(word.id)}>"{word.title}"</span>
+                ))}
+              </div>
+          </Section>
+
+          <Section>
+              <h2>Sugestões</h2>
+              <div>
+                {sugestions && sugestions.map(sugestion => (
+                  <span key={String(sugestion.id)}>"{sugestion.title}"</span>
+                ))}
+              </div>
+          </Section>
+        </div>
+
+        <Form>
+            <h3>Achou alguma ofensa que não está aqui? <br/> <span>Deixe sua sugestão</span></h3>
+                  
+            <div className="inputWrapper">
+            <Input placeholder="Deixe sua sugestão aqui" onChange={(e: any) => setTitle(e.target.value)}/>
+            <Button text="Enviar" onClick={handleSendSugestion}/>
+            </div>
+        </Form>
+      </Main>
+
     </Container>
   )
 }
--- src/pages/ApiInfo/styles.ts
+++ src/pages/ApiInfo/styles.ts
@@ -8,13 +8,12 @@ export const Container = styled.div`
     grid-template-rows: 100px auto;
     grid-template-areas:
     "header"
-    "content"
+    "main"
     ;
 `
 
 export const Header = styled.div`
     grid-area: header;
-    width: 100%;
 
     padding: 20px;
 
@@ -23,12 +22,106 @@ export const Header = styled.div`
     align-items: center;
     justify-content: space-between;
 
+    gap: 5px;
+
 
     h1{
         font-family: 'JetBrains Mono';
         font-weight: 700;
-        font-size: 36px;
+        font-size: 28px;
         line-height: 40px;
     }
 `
 
+export const Main = styled.div`
+    grid-area: main;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+
+    padding: 0 20px;
+
+    h1{
+        text-align: center;
+        font-size: 24px;
+        font-family: 'JetBrains Mono';
+        color: var(--green);
+    }
+
+    .Sections{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        gap: 10px;
+        width: 100%;
+    }
+
+`
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+
+    width: 100%;
+
+
+    h2{
+        font-size: 15px;
+        width: 100%;
+        text-align: center;
+    }
+
+    div{
+        background-color: #222042;
+
+        height: 150px;
+        min-width: 150px;
+        max-width: 500px;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+
+        border-radius: 10px;
+
+        padding: 15px;
+
+        overflow-y: auto;
+
+        font-family: 'JetBrains Mono';
+    }
+
+`
+
+
+export const Form = styled.div`
+    font-family: 'JetBrains Mono';
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+
+    h3, span{
+        text-align: center;
+    }
+
+    .inputWrapper{
+        display: flex;
+
+        input{
+            border-radius: 10px 0 0 10px;
+        }
+        button{
+            border-radius: 0 10px 10px 0;
+            background-color: #222042;
+            color: white;
+        }
+    }
+
+
+`
