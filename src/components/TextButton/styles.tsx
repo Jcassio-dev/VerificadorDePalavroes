aa3fa07f2157311a7ceb1414879398c423573121
José Cássio <josecassio2013@gmail.com>
2023-04-13 08:53:33 -0300

    refactored textbutton component for cleaner code

---
+++ src/components/TextButton/styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const Container = styled.button`
+    background: none;
+    border: none;
+
+    color: var(--white);
+
+    cursor: pointer;
+
+    a{
+        text-decoration: none;
+        color: var(--white);
+    }
+
+`
