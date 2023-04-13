5c472461aed9a36f3e3ff02e6e46b1f17592560f
José Cássio <josecassio2013@gmail.com>
2023-04-13 08:35:25 -0300

    add api consult

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 import { Container, Info } from "./styles";
+
+import { api } from '../../services/api'
 
 import { badWordList } from "../../utils/badWordList";
 
@@ -10,18 +12,24 @@ import Security from '../../assets/Security.svg';
 import { Textarea } from "../../components/Textarea";
 import { Button } from "../../components/Button";
 
+interface  BadWordInterface{
+  id: number;
+  title: string;
+  created_at: string;
+}
 
 export function Home() {
   const [text, setText] = useState('');
+  const [words, setWords] = useState<BadWordInterface[]>([])
 
   const verifyText = (text: string) => {
     const badWords: string[] = [];
 
     let formatText = text.toLowerCase();
     
-    const isInclude = badWordList.some(word => {
-      if(formatText.includes(word)){
-        badWords.push(word)
+    const isInclude = words.some(word => {
+      if(formatText.includes(word.title)){
+        badWords.push(word.title)
       }   
     });
 
@@ -33,6 +41,14 @@ export function Home() {
     const response = badWords.length > 0 ? toast.error(`Conteúdo Impróprio: ${badWords}`) : toast.success('Texto seguro!');
    
   }
+
+  useEffect(() => {
+    async function fetchWords(){
+       await api.get("/words?title=").then(({data}) => setWords(data));
+    }
+    fetchWords();
+    console.log(words)
+  }, [])
 
   return(
     <Container>
