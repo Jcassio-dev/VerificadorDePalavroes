f92e71131ac5b7bd1fd0c690dec6eb9ee8b7b128
José Cássio <josecassio2013@gmail.com>
2023-04-14 12:37:05 -0300

    create Loading Component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import {Container} from './styles'
+
+3
+export const Loading = () => {
+    return (
+        <Container/>
+    );
+};
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    animation: is-rotating 1s infinite;
+    border: 6px solid var(--white);
+    border-radius: 50%;
+    border-top-color: var(--green);
+    height: 25px;
+    width: 25px;
+  `
